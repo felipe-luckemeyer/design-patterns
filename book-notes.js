9e99f4a001dc08e2e374002339791e4fe67524a2
Felipe <felipe.luckemeyer10@gmail.com>
2022-11-22 22:38:21 -0300

    design patter definition

---
+++ book-notes.js
@@ -0,0 +1,16 @@
+// Design Patterns: Elements of Reusable Object Oriented Software
+
+// Read chapters 1 and 2 (explain about design patters):
+
+// What is a Design Patterns?
+// - A problem which occurs a lot of times in our environment and the possibility to create a solution that can be used several times over.
+
+// After that, learn the following patterns:
+// - Abstract Factory (page 87)
+// - Factory Method (page 107)
+// - Adapter (page 139)
+// - Composite (page 163)
+// - Decorator (page 175)
+// - Observer (page 293)
+// - Strategy (page 315)
+// - Template Method (page 325)
