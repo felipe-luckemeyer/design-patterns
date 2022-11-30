e384789ef26f6f4871a3393217025a51d1f3a0c3
Felipe <felipe.luckemeyer10@gmail.com>
2022-11-29 21:28:10 -0300

    design patter definition

--- book-notes.js
+++ book-notes.js
@@ -2,15 +2,20 @@
 
 // Read chapters 1 and 2 (explain about design patters):
 
-// What is a Design Patterns?
+// What is a Design Pattern?
 // - A problem which occurs a lot of times in our environment and the possibility to create a solution that can be used several times over.
+
+// Patterns can be separated in three different purposes:
+// - Creational: concern the process of object creation;
+// - Strutural: deal with the composition of classes or objects;
+// - Behavioral: characterize the ways in which classes or objects interact and distribute responsibility.
 
 // After that, learn the following patterns:
 // - Abstract Factory (page 87)
 // - Factory Method (page 107)
 // - Adapter (page 139)
-// - Composite (page 163)
+// - Composite (page 163) => MVC
 // - Decorator (page 175)
-// - Observer (page 293)
-// - Strategy (page 315)
+// - Observer (page 293) => MVC
+// - Strategy (page 315) => MVC
 // - Template Method (page 325)
